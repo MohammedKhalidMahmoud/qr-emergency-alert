6ea41793b522551c461e3aaa56a21efea1d45349
mkhaled <mohammedkhalidmahmoud@gmail.com>
2026-08-28 18:59:28 +0200

    add firebase config.

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,12 +1,12 @@
 (function (global) {
   global.FIREBASE_CONFIG = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
-  };
+  apiKey: "AIzaSyA1XO1HDb6Q03KBdNm9oZ-WkfHRriD0p64",
+  authDomain: "qrcode-emergency-alert.firebaseapp.com",
+  projectId: "qrcode-emergency-alert",
+  storageBucket: "qrcode-emergency-alert.firebasestorage.app",
+  messagingSenderId: "683768433398",
+  appId: "1:683768433398:web:3985795902dbf60c5bbad7"
+};
 
   global.FIREBASE_VAPID_KEY = '';
 
