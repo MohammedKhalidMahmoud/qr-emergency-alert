5ccd04cb49aa51a15fc0756e625227b9b6143b91
mkhaled <mohammedkhalidmahmoud@gmail.com>
2026-08-29 21:31:50 +0200

    elite

--- server.js
+++ server.js
@@ -287,7 +287,7 @@ function corsMiddleware(req, res, next) {
   }
 
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, ngrok-skip-browser-warning');
 
   if (req.method === 'OPTIONS') {
     res.status(204).end();
